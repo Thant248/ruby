bb343c45feea00722253b6f37f8819b2690f129b
ASUS <thantsinphyoe18920@gmail.com>
2024-01-22 21:18:58 +0630

    .

---
+++ RubyFundenmental/@12DateTime.rb
@@ -0,0 +1,11 @@
+time =  Time.new
+
+if(time.wday == 1 || time.wday == 2 || time.wday == 3 || time.wday == 4 || time.wday == 5)
+    puts "頑張って下さい。。。"
+elsif (time.wday == 6)
+     puts "土曜日"
+else
+    puts "日曜日"
+    
+end
+
